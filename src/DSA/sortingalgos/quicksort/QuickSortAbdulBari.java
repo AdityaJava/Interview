d68e915e5af1f008a3599c1cfca7b67c8ddb7bbf
Aditya <adityadudhal97@gmail.com>
2024-07-09 10:47:29 +0530

    Quick Sort working as expected

--- src/DSA/sortingalgos/quicksort/QuickSortAbdulBari.java
+++ src/DSA/sortingalgos/quicksort/QuickSortAbdulBari.java
@@ -1,31 +1,38 @@
 package DSA.sortingalgos.quicksort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class QuickSortAlgo2 {
+public class QuickSortAbdulBari {
 
     private static void quickSort(List<Integer> list, int low, int high) {
         if (low < high) {
             int pi = partition(list, low, high);
-            quickSort(list, low, pi - 1);
+
+            quickSort(list, low, pi);
             quickSort(list, pi + 1, high);
         }
-
     }
 
     private static int partition(List<Integer> list, int low, int high) {
-        int pivot = list.get(high);
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (list.get(j) <= pivot) {
+        int pivot = list.get(low);
+        int i = low, j = high;
+        while (i < j) {
+            do {
                 i++;
-                swap(list, j, i);
+            } while (i < high && (list.get(i) <= pivot));
+
+            do {
+                j--;
+            } while (j > low && (list.get(j) > pivot));
+
+            if (i < j) {
+                swap(list, i, j);
             }
         }
-        swap(list, i + 1, high);
-        return i + 1;
+        swap(list, low, j);
+        return j;
     }
 
     private static void swap(List<Integer> list, int pos1, int pos2) {
@@ -35,14 +42,12 @@ public class QuickSortAlgo2 {
     }
 
     public static void main(String[] args) {
-//        List<Integer> list = Arrays.asList(1, 4, 2, 9, 3, 5, 100, 96);
-        List<Integer> list = Arrays.asList(2, 1, 10);
-
+//        List<Integer> list = Arrays.asList(2, 1, 10);
+        Integer infinity = 9999;
+        List<Integer> list = Arrays.asList(1, 4, 2, 9, 3, 5, 100, 96, 110, 105, infinity);
         quickSort(list, 0, list.size() - 1);
 
         list.forEach(e -> System.out.println(e));
 
     }
-
-
 }
